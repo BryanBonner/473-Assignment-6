ff3180345898ca45b49cbc982964a120e62b2050
BryanBonner <bonnerbryan@gmail.com>
2016-11-27 16:53:27 -0800

    jshint

--- public/js/app.js
+++ public/js/app.js
@@ -1,56 +1,22 @@
+/* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
 var main = function () {
-	"using strict"
+	'use strict';
 
 	var socket = io();
-	var url = "http://localhost:3000/trivia";
-	var question, userAnswer, score,
+	var url = 'http://localhost:3000/trivia';
+	var question, score,
 	    qObj = {id: null, question: null}, connectCount = 0,
-	    answeredCount = 0, usedQuestions = [], scoreObj = {}, users = [];
-
-	// SOCKET IO .on
-	socket.on('user join', function(msg) {
-		connectCount++;
-		users.push(new getUserName(msg));
-		TriviaViewModel.userList(users);
-	});
-	socket.on('game start', function(question) {
-		TriviaViewModel.question(question.question);
-		TriviaViewModel.startButton(false);
-	});
-	socket.on('get question id', function(question) {
-		//Pass the question id to ALL clients in order to correctly POST answer for
-		// each connected client
-		qObj.id = question.answerid;
-	});
-
-	// Keep track of the amount of connected users who answer in order to find the
-	// next question once everyone has answered
-	socket.on('user answers', function(msg) {
-		answeredCount++;
-		TriviaViewModel.userOutcome(" " + msg.correct);
-		if(answeredCount == connectCount) {
-			answeredCount = 0;
-			retrieveScore();
-			TriviaViewModel.updateQuestion();
-			TriviaViewModel.submitButton(true);
-		}
-	});
-
-	socket.on('user scores', function(scoreObj) {
-		TriviaViewModel.rightScore(scoreObj.score[0]);
-		TriviaViewModel.wrongScore(scoreObj.score[1]);
-	});
-	// END SOCKET IO
+	    answeredCount = 0, users = [];
 
 	// Knockout View Model
 	var TriviaViewModel = {
-		userName: ko.observable(""),
+		userName: ko.observable(''),
 		userList: ko.observableArray([]),
-		question: ko.observable(""),
-		userAnswer: ko.observable(""),
-		userOutcome: ko.observable(""),
-		rightScore: ko.observable("0"),
-		wrongScore: ko.observable("0"),
+		question: ko.observable(''),
+		userAnswer: ko.observable(''),
+		userOutcome: ko.observable(''),
+		rightScore: ko.observable('0'),
+		wrongScore: ko.observable('0'),
 		joinButton: ko.observable(true),
 		startButton: ko.observable(true),
 		submitButton: ko.observable(true),
@@ -68,7 +34,7 @@ var main = function () {
 
 	//Self method copied from the Knockout tutorial of collections,
 	// helps with scope issues is my understanding
-	function getUserName(userName) {
+	function GetUserName(userName) {
 		var self = this;
 		self.userName = userName;
 	}
@@ -87,25 +53,8 @@ var main = function () {
 				socket.emit('get question id', question);
 				socket.emit('game start', question);
 			},
-			fail: function(question) {
+			fail: function() {
 				alert('failed to retrieve question');
-			}
-		});
-	};
-
-	// Get the score and emit to all users
-	function retrieveScore() {
-		$.ajax({
-			type: 'GET',
-			url: url + '/score',
-			contentType: 'application/json',
-			dataType: 'json',
-			data: score,
-			success: function(score) {
-				socket.emit('user scores', score);
-			},
-			fail: function(score) {
-				alert('failed to retrieve score');
 			}
 		});
 	}
@@ -114,14 +63,14 @@ var main = function () {
 	// then update the view model and hide the submit button until all users
 	//   have answered
 	function retrieveAnswerOutcome() {
-		if(TriviaViewModel.userAnswer() == "") {
-			alert("Enter an answer before submitting!");
+		if(TriviaViewModel.userAnswer() === '') {
+			alert('Enter an answer before submitting!');
 		}
-		else if(TriviaViewModel.userList() == "") {
-			alert("There are no active players, join competition first!");
+		else if(TriviaViewModel.userList() === '') {
+			alert('There are no active players, join competition first!');
 		}
-		else if(qObj.question == null && qObj.id == null) {
-			alert("Start the game first");
+		else if(qObj.question === null && qObj.id === null) {
+			alert('Start the game first');
 		}
 		else {
 			TriviaViewModel.submitButton(false);
@@ -138,6 +87,58 @@ var main = function () {
 			});
 		}
 	}
+	// Get the score and emit to all users
+	function retrieveScore() {
+		$.ajax({
+			type: 'GET',
+			url: url + '/score',
+			contentType: 'application/json',
+			dataType: 'json',
+			data: score,
+			success: function(score) {
+				socket.emit('user scores', score);
+			},
+			fail: function() {
+				alert('failed to retrieve score');
+			}
+		});
+	}
+
+// SOCKET IO .on
+	socket.on('user join', function(msg) {
+		connectCount++;
+		users.push(new GetUserName(msg));
+		TriviaViewModel.userList(users);
+	});
+	socket.on('game start', function(question) {
+		TriviaViewModel.question(question.question);
+		TriviaViewModel.startButton(false);
+	});
+	socket.on('get question id', function(question) {
+		//Pass the question id to ALL clients in order to correctly POST answer
+		// for each connected client
+		qObj.id = question.answerid;
+	});
+
+	// Keep track of the amount of connected users who answer in order to find
+	// the next question once everyone has answered
+	socket.on('user answers', function(msg) {
+		answeredCount++;
+		TriviaViewModel.userOutcome(' ' + msg.correct);
+		if(answeredCount === connectCount) {
+			answeredCount = 0;
+			retrieveScore();
+			TriviaViewModel.updateQuestion();
+			TriviaViewModel.submitButton(true);
+		}
+	});
+
+	socket.on('user scores', function(scoreObj) {
+		TriviaViewModel.rightScore(scoreObj.score[0]);
+		TriviaViewModel.wrongScore(scoreObj.score[1]);
+	});
+	// END SOCKET IO
+
 	ko.applyBindings(TriviaViewModel);
 };
 
